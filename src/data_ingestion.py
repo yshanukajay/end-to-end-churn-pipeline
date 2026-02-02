18afe1a31037ef0ec6c90269beeb556146bbd9a4
yshanukajay <yshanukajay@gmail.com>
2026-02-02 14:17:29 +0530

    data ingestion completed

---
+++ src/data_ingestion.py
@@ -0,0 +1,22 @@
+import os
+import pandas as pd
+from abc import ABC, abstractmethod
+
+class DataIngester(ABC):
+    @abstractmethod
+    def ingest_data(self, file_path_or_link: str) -> pd.DataFrame:
+        pass
+    
+class DataIngestorCSV(DataIngester):
+    def ingest_data(self, file_path_or_link: str) -> pd.DataFrame:
+        try:
+            return pd.read_csv(file_path_or_link)
+        except Exception as e:
+            raise RuntimeError(f"Error ingesting CSV data: {e}")
+        
+class DataIngestorExcel(DataIngester):
+    def ingest_data(self, file_path_or_link: str) -> pd.DataFrame:
+        try:
+            return pd.read_excel(file_path_or_link)
+        except Exception as e:
+            raise RuntimeError(f"Error ingesting Excel data: {e}")
