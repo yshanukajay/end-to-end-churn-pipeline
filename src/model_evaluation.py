b83dd834ef75e2e230be20298aff72f681069ee4
yshanukajay <yshanukajay@gmail.com>
2026-02-17 20:10:04 +0530

    model evaluation started

---
+++ src/model_evaluation.py
@@ -0,0 +1,24 @@
+import os
+import logging
+import warnings
+from typing import Dict, Any, Optional, Union
+from datetime import datetime
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+    confusion_matrix
+)
+
+warnings.filterwarnings("ignore")
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+logger = logging.getLogger(__name__)
